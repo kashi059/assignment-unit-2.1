5d88f7d28f4b1848a058fb90316c36bffa4049c1
Joe Wipperfurth <wipperfurth059@gmail.com>
2023-07-30 11:46:12 -0500

    fixed the pets variable on line 26

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -23,7 +23,7 @@ let pets = 0;
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
 let friendsPets = 1;
 // 11 - Add two pets to your `pets` variable
-let pets = pets + 2;
+pets = pets + 2;
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 const allowedPets = 5;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
